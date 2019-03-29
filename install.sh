03befc9a6838ab8c683438da9c8985afaf0f4e76
Арсений Шестаков <ptath@ptath.ru>
2019-03-29 21:59:24 +0400

    Now installing script directly from rclone.org, added questions

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version 0.1
+# Version 0.5
 # By ptath (https://ptath.ru)
 # rclone installation script
 
@@ -50,12 +50,9 @@ print_title() {
 
 print_title "rclone installation script" "Press CTRL+C anytime to abort"
 
-[ -e /bin/rclone ] && echo " Already installed (/bin/rclone)? Exiting..." && exit
-[ -e /usr/bin/rclone ] && echo " Already installed (usr/bin/rclone)? Exiting..." && exit
+curl https://rclone.org/install.sh | sudo bash
 
-sudo apt install rclone
-
-[ -e ~/scripts/backup.sh ] && echo " Script already installed (~/scripts/backup.sh)? Exiting..." && exit
+[ -e ~/scripts/backup.sh ] && echo " Script already installed ($(print_cyan "~/scripts/backup.sh"))? Exiting..." && exit
 
 [ ! -d ~/scripts ] && mkdir ~/scripts
 [ ! -d ~/.rclone ] && mkdir ~/.rclone
@@ -65,32 +62,34 @@ wget -q -O ~/.rclone/backup.list https://github.com/ptath/pi-rclone/raw/master/.
 wget -q -O ~/.rclone/excludes.list https://github.com/ptath/pi-rclone/raw/master/.rclone/excludes.list
 
 chmod +x ~/scripts/backup.sh
-echo " Downloaded script to ~/scripts directory, here is its content:"
-cd ~/scripts && ls -a
 
-echo " Configuring rclone..."
-rclone config
+read -p " Configure rclone now? (y/N)? " yn
+case $yn in
+  [Yy]* )
+    rclone config
+  ;;
+  * )
+    echo " OK, skipping config..."
+  ;;
+esac
 
-echo "  Editing .rclone/backup.list in vi (press $(print_cyan "i") to edit mode),"
-echo "    add directories to backup,"
-echo "    then press ESC and type $(print_cyan ":wq") then ENTER)"
-read -p "   Press ENTER to proceed..."
-vi ~/.rclone/backup.list
+echo " "
+echo " Please review following files and edit them if necessary: "
+echo " "
+echo "  ~/.rclone/backup.list   — List of directories to backup"
+echo "  ~/.rclone/excludes.list — Exclude special files or directories from backup"
+echo "  ~/scripts/backup.sh     — Insert yours service name and path to backup"
+echo " "
 
-echo "  Editing .rclone/excludes.list in vi (press $(print_cyan "i") to edit mode),"
-echo "    add file types to exclude from backup,"
-echo "    then press ESC and type $(print_cyan ":wq") then ENTER)"
-read -p "   Press ENTER to proceed..."
-vi ~/.rclone/excludes.list
-
-echo "  Editing ~/scripts/backup.sh in vi (press $(print_cyan "i") to edit mode),"
-echo "    add file types to exclude from backup,"
-echo "    then press ESC and type $(print_cyan ":wq") then ENTER)"
-read -p "   Press ENTER to proceed..."
-vi ~/scripts/backup.sh
-
-echo " Installing backup cron job in 04:15 every day"
-(crontab -l ; echo "30 4 * * * ~/scripts/backup.sh > /dev/null 2>&amp;1") | crontab -
-echo " Installed, run $(print_cyan "crontab -e") to edit"
+read -p " Install $(print_cyan "cron job") at 04:15 every day (y/N) ? " yn
+case $yn in
+  [Yy]* )
+    (crontab -l ; echo "30 4 * * * ~/scripts/backup.sh > /dev/null 2>&amp;1") | crontab -
+    echo " Installed, run $(print_cyan "crontab -e") to edit"
+  ;;
+  * )
+    exit
+  ;;
+esac
 
 print_title "That's all!" "Run ~/scripts/backup.sh"
